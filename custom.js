aad7316e11227bff299bab1660628b28c704108f
Aswankappingal <instamagic774@gmail.com>
2024-01-01 14:51:02 +0530

    file added

---
+++ custom.js
@@ -0,0 +1,14 @@
+ let flag="0"
+function color(){
+if(flag=="0"){
+    $("body").css({"background-color":"red"})
+    flag="1"
+
+}
+ else{
+    $("body").css({"background-color":"green"})
+    flag="0"
+}
+
+   
+}
